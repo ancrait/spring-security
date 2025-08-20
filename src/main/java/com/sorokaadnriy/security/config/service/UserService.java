0019098689b12716feb1dfe8af4f01a0e1a482b6
ancrait <xdxddclash@gmail.com>
2025-08-20 13:36:45 +0300

    add authenticationManager

--- src/main/java/com/sorokaadnriy/security/config/service/UserService.java
+++ src/main/java/com/sorokaadnriy/security/config/service/UserService.java
@@ -3,6 +3,9 @@ package com.sorokaadnriy.security.config.service;
 import com.sorokaadnriy.security.entity.User;
 import com.sorokaadnriy.security.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -12,11 +15,13 @@ public class UserService {
 
     private final UserRepository repository;
     private final BCryptPasswordEncoder encoder;
+    private final AuthenticationManager authenticationManager;
 
     @Autowired
-    public UserService(UserRepository repository, BCryptPasswordEncoder encoder) {
+    public UserService(UserRepository repository, BCryptPasswordEncoder encoder, AuthenticationManager authenticationManager) {
         this.repository = repository;
         this.encoder = encoder;
+        this.authenticationManager = authenticationManager;
     }
 
 
@@ -26,7 +31,10 @@ public class UserService {
     }
 
     public String login(User user) {
-        if (repository.findByUserName(user.getUserName()).isPresent()){
+       Authentication authentication =  authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.getUserName(),user.getPassword()));
+
+        if (authentication.isAuthenticated()){
             return "success";
         }
         throw new UsernameNotFoundException("Check password or username again");
